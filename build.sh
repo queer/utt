31910244d36e15218d01c643b254be1a3b52a167
amy null <null@amy.gg>
2022-02-28 17:32:50 -0500

    🎨 style: :D

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 mvn clean package
-rm target/utt
+rm -fv target/utt
 echo "#!/usr/bin/env -S java -jar" >> target/utt
 cat target/utt-*.jar >> target/utt
 chmod +x target/utt
 echo ">> Done!"
+echo ">> Install ./target/utt to your \$PATH"
+echo ">> :D"
